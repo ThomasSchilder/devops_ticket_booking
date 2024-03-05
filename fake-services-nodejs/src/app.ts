27deaf8e1073928a4ccdfd2eeb0f38cf5a3ad002
jojoJochem <jochemgvg@gmail.com>
2024-03-05 13:43:00 +0100

    update

--- fake-services-nodejs/src/app.ts
+++ fake-services-nodejs/src/app.ts
@@ -3,28 +3,28 @@ const { v4: uuidv4 } = require('uuid');
 ////////////////////////////////////
 // FAKE SEAT RESERVATION SERVICE
 ////////////////////////////////////
-// import { ZBClient } from "zeebe-node";
-// require("dotenv").config();
+import { ZBClient } from "zeebe-node";
+require("dotenv").config();
 
-// const zeebeClient = new ZBClient();
-// const worker = zeebeClient.createWorker('reserve-seats', reserveSeatsHandler)
+const zeebeClient = new ZBClient();
+const worker = zeebeClient.createWorker('reserve-seats', reserveSeatsHandler)
 
-// function reserveSeatsHandler(job, _, worker) {
-//   console.log("\n\n Reserve seats now...");
-//   console.log(job);
+function reserveSeatsHandler(job, _, worker) {
+  console.log("\n\n Reserve seats now...");
+  console.log(job);
 
-//   // Do the real reservation
-//   // TODO: Fake some results! Fake an error (when exactly?)
-//   if ("seats" !== job.variables.simulateBookingFailure) {
-//     console.log("Successul :-)");
-//     return job.complete({
-//         reservationId: "1234",
-//       });
-//   } else {
-//     console.log("ERROR: Seats could not be reserved!");
-//     return job.error("ErrorSeatsNotAvailable");
-//   }
-// }
+  // Do the real reservation
+  // TODO: Fake some results! Fake an error (when exactly?)
+  if ("seats" !== job.variables.simulateBookingFailure) {
+    console.log("Successul :-)");
+    return job.complete({
+        reservationId: "1234",
+      });
+  } else {
+    console.log("ERROR: Seats could not be reserved!");
+    return job.error("ErrorSeatsNotAvailable");
+  }
+}
 
 
 
