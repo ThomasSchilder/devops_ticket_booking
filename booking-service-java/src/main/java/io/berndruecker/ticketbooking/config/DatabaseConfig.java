783c9800ccefecc3adbc661e9eae6933537839b4
jojoJochem <jochemgvg@gmail.com>
2024-03-18 13:14:21 +0100

    update dockerfile for properties passing

---
+++ booking-service-java/src/main/java/io/berndruecker/ticketbooking/config/DatabaseConfig.java
@@ -0,0 +1,32 @@
+package io.berndruecker.ticketbooking.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConfig {
+    @Value("${DATABASE_PASSWORD}")
+    private String databasePassword;
+    // use the databasePassword variable as needed
+    @Value("${ZEEBE_CLIENT_CLOUD_REGION}")
+    private String zeebeClientCloudRegion;
+    @Value("${ZEEBE_CLIENT_CLOUD_CLUSTERID}")
+    private String zeebeClientCloudClusterId;
+    @Value("${ZEEBE_CLIENT_CLOUD_CLIENTID}")
+    private String zeebeClientCloudClientId;
+    @Value("${ZEEBE_CLIENT_CLOUD_CLIENTSECRET}")
+    private String zeebeClientCloudClientSecret;
+    @Value("${SPRING_RABBITMG_ADRESSES}")
+    private String springRabbitmgAdresses;
+    @Value("${SPRING_RABBITMG_HOST}")
+    private String springRabbitmgHost;
+    @Value("${SPRING_RABBITMG_USERNAME}")
+    private String springRabbitmgUsername;
+    @Value("${SPRING_RABBITMG_PORT}")
+    private String springRabbitmgPort;
+    @Value("${SPRING_RABBITMG_PASSWORD}")
+    private String springRabbitmgPassword;
+    @Value("${SPRING_RABBITMG_VIRTUAL_HOST}")
+    private String springRabbitmgVirtualHost;
+    @Value("${SPRING_RABBITMG_SSL_ENABLED}")
+    private String springRabbitmgSslEnabled;
+}
