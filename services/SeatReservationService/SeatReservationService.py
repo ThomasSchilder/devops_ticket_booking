6321cd795a84a5c08e3642412ffe93fcc360d551
Ubuntu <ubuntu@ip-10-0-14-193.eu-north-1.compute.internal>
2024-03-20 11:25:53 +0000

    Correct lamdba functions with live variant

--- services/SeatReservationService/SeatReservationService.py
+++ services/SeatReservationService/SeatReservationService.py
@@ -1,18 +1,16 @@
 import json
 
-
 def lambda_handler(event, context):
+    # Throw 'ErrorSeatsNotAvailable' whenever a booking failure is simulated.
+    if (event['simulateBookingFailure'] == 'seats'):
+        raise Exception("ErrorSeatsNotAvailable")
+
     booking_reference_id = event["bookingReferenceId"]
-    if ("seat" != booking_reference_id):
-        reservation = "1234"
-        return {
-            'statusCode': 200,
-            "headers": {
-                "content-type": "application/json"
-            },
-            'body': json.dumps(reservation)
-        }
+    reservation = "1234"
     return {
-        'statusCode': 400,
-        'body': json.dumps("ErrorSeatsNotAvailable")
+        'statusCode': 200,
+        "headers": {
+            "content-type": "application/json"
+        },
+        'body': json.dumps(reservation)
     }
--- services/paymentRetrievedService/index.mjs
+++ services/paymentRetrievedService/index.mjs
@@ -1,42 +1,84 @@
-import amqp from 'amqplib/callback_api.js';
-// import { v4 as uuidv4 } from 'crypto/callback_api.js';
+import amqp from 'amqplib';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
-const queuePaymentRequest = 'paymentRequest';
-const queuePaymentResponse = 'paymentResponse';
-const username = "developer2" //process.env.username;
-const password = "giYZB5m%jM3KKakD"//process.env.password;
 
-export async function handler (event) {
-    return new Promise((resolve, reject) => {
-    var encodedPassword = encodeURIComponent(password);
-    amqp.connect("amqps://"+username+":"+encodedPassword+"@b-258577ee-ad63-431b-9f94-005160961adf.mq.eu-north-1.amazonaws.com:5671", function(error0, connection){
-        if (error0) {
-            reject(error0)
-            return;
-        }
-        connection.createChannel(function (error1, channel) {
-            if (error1) {
-                reject(error1)
-                return;
-            }
-            channel.assertQueue(queuePaymentRequest, { durable: true });
-            channel.assertQueue(queuePaymentResponse, { durable: true });
-            channel.consume(queuePaymentRequest, function (inputMessage) {
-                const paymentRequestId = inputMessage.content.toString();
-                const paymentConfirmationId = "CONFIRMED";
-                // const paymentConfirmationId = uuidv();
-                console.log(`\n\n [x] Received payment request ${paymentRequestId}`);
-                const outputMessage = JSON.stringify({
-                    paymentRequestId: paymentRequestId,
-                    paymentConfirmationId: paymentConfirmationId
-                });
-                console.log(outputMessage);
-                channel.sendToQueue(queuePaymentResponse, Buffer.from(outputMessage));
-                console.log(` [x] Sent payment response ${outputMessage}`);
-            }, {
-                noAck: true
-            });
+/**
+ * This function creates a connection with a RabbitMQ broker using the credentials stored in the AWS Secret Manager.
+ */
+async function createConnection() {
+        // Query RabbitMQ credentials from AWS Secret Manager
+        const client = new SecretsManagerClient({
+            region: "eu-north-1",
         });
-    });
-    });
+        const secret_name = "lambda-mq-access";
+        let response;
+        try {
+            response = await client.send(
+                new GetSecretValueCommand({
+                    SecretId: secret_name,
+                    VersionStage: "AWSCURRENT"
+                })
+            );
+        } catch (error) {
+            console.log(error)
+            throw error;
+        }
+        const secret = JSON.parse(response.SecretString);
+
+        // Parse secret object
+        let username = secret.username;
+        let password = encodeURI(secret.password);
+        let host = secret.host;
+        let port = secret.port;
+
+        // Create connection
+        const conn = await amqp.connect(`amqps://${username}:${password}@${host}:${port}`);
+        return conn;
+}
+
+export const handler = async (event) => {
+    // Retrieve messages from event variable.
+    let messages = event["rmqMessagesByQueue"]["paymentRequest::/"];
+    console.log(`Found ${messages.length} messages`);
+
+
+    // Start connection to send message to respond queue.
+    let conn = await createConnection();
+
+    // Connect to queue.
+    const responseQueue = 'paymentResponse';
+    const channel = await conn.createChannel();
+    console.log(`Connected to queue '${responseQueue}'`);
+
+    for (let message of messages) {
+        // Parse message and check if id is given.
+        let data = message.data;
+        if (data) {
+            // Transform buffer to (utf-8) string.
+            const id = Buffer.from(data, 'base64').toString('utf8');
+
+            // Create a message response.
+            const response = JSON.stringify({
+                paymentRequestId: id,
+                paymentConfirmationId: uuidv4()
+            });
+
+            // Send response.
+            channel.sendToQueue(responseQueue, Buffer.from(response));
+            console.log(`Sent response to '${responseQueue}'`);
+        }
+
+    }
+
+    // Close channel.
+    console.log("Closing channel");
+    await channel.close();
+
+    // Close connection.
+    console.log("Closing connection");
+    await conn.close();
 };
